aa0a1273e211655de46ce8829d7720affba4ebd5
Max1mShvedov <111134552+Max1mShvedov@users.noreply.github.com>
2023-04-10 21:58:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('menu__list-active');
+});
+
+
+
+  const swiper = new Swiper(".mySwiper", {
+effect: "fade",
+pagination: {
+  el: ".swiper-pagination",
+},
+  autoplay: {
+  delay: 3000,
+  disableOnInteraction: false,
+},});
